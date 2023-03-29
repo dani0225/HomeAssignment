18eee9dd4ade78eb121bfd4266873fb289b951a1
Daniel <daniel.ardeleanu89@gmail.com>
2023-03-29 12:36:58 +0300

    Initial Commit

---
+++ TakeHomeAssignment/Views/ContactsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TakeHomeAssignment
+//
+//  Created by Daniel Ardeleanu on 29.03.2023.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ TakeHomeAssignment/Views/WeatherAlertsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherAlertsViewController.swift
+//  TakeHomeAssignment
+//
+//  Created by Daniel Ardeleanu on 29.03.2023.
+//
+
+import UIKit
+
+class WeatherAlertsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
